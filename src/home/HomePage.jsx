0cc315993daaafdc76b7fe7b6b3b98722e4375a5
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-11-11 20:52:31 +0600

    Section Homepage

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,5 +1,17 @@
+import Banner from "./banner/Banner";
+import Categories from "./categories/Categories";
+import Featured from "./feature/Featured";
+import PopularMenu from "./popular-menu/PopularMenu";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Banner />
+      <Categories />
+      {/* <PopularMenu /> */}
+      <Featured />
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ src/hooks/useMenu.jsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+
+const useMenu = () => {
+  // const [menu, setMenu] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //     fetch('https://bistro-boss-server-fawn.vercel.app/menu')
+  //         .then(res => res.json())
+  //         .then(data => {
+  //             setMenu(data);
+  //             setLoading(false);
+  //         });
+  // }, [])
+
+  const {
+    data: menu = [],
+    isLoading: loading,
+    refetch,
+  } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://bistro-boss-server-fawn.vercel.app/menu"
+      );
+      return res.json();
+    },
+  });
+
+  return [menu, loading, refetch];
+};
+
+export default useMenu;
