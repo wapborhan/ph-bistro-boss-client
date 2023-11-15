e056b45557cebc451cf463dd567149fc6396287d
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-11-16 00:26:51 +0600

    server add

--- src/home/Testimonials/Testimonials.jsx
+++ src/home/Testimonials/Testimonials.jsx
@@ -13,7 +13,7 @@ const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
-    fetch("reviews.json")
+    fetch("http://localhost:3300/reviews")
       .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);
--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -5,7 +5,7 @@ const useMenu = () => {
   const [menu, setMenu] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch("/menu.json")
+    fetch("http://localhost:3300/menu")
       .then((res) => res.json())
       .then((data) => {
         setMenu(data);
